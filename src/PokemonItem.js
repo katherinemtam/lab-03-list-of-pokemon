c63fcad9873a282f9522f1d472751509d2c78a70
Katherine Tam <katherinemtam@gmail.com>
2021-04-28 15:52:42 -0700

    add PokemonList & Pokemon Item files

---
+++ src/PokemonItem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './PokemonItem.css';
+
+class PokemonItem extends Component {
+  state = {}
+  render() {
+    return (  );
+  }
+}
+
+export default PokemonItem;
---
+++ src/PokemonList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './PokemonList.css';
+
+class PokemonList extends Component {
+  state = {}
+  render() {
+    return (  );
+  }
+}
+
+export default PokemonList;
