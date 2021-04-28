634b2e9704232afb3c25a4a46c7ee9ca6bb867d9
Katherine Tam <katherinemtam@gmail.com>
2021-04-28 16:02:10 -0700

    connect pokemon API

--- src/App.js
+++ src/App.js
@@ -1,19 +1,41 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import PokemonList from './PokemonList';
+import request from 'superagent';
 import './App.css';
-import React from 'react';
+
+const POKEMON_API_URL = 'https://pokedex-alchemy.herokuapp.com/api/pokedex';
 
 class App extends Component {
 
+  state = {
+    pokemon: []
+  }
+
+  async componentDidMount() {
+    const response = await request.get(POKEMON_API_URL);
+  }
+
+
   render() {
+
+    const { pokemon } = this.state;
+
     return (
       <div className="App">
 
         <Header />
 
+        <section className="search-options">
+
+        </section>
+
         <main>
+
           Something should appear here
+          <PokemonList pokemon={pokemon} />
+
         </main>
 
         <Footer />
