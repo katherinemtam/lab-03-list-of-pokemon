6d97bcf9516fee961037af22eb99b137565ceab9
Katherine Tam <katherinemtam@gmail.com>
2021-04-29 09:02:48 -0700

    working search, add HP filter

--- src/app/App.js
+++ src/app/App.js
@@ -12,21 +12,59 @@ class App extends Component {
 
   state = {
     pokemon: [],
-    nameSearch: '',
+    staticPokemon: [],
+    hp: [],
   }
 
   async componentDidMount() {
-    const response = await request.get(POKEMON_API_URL);
-    this.setState({ pokemon: response.body.results });
+    this.fetchVillagers();
   }
 
-  handleSearch = ({ nameSearch }) => {
-    console.log(nameSearch);
+  async fetchVillagers() {
+    const { nameSearch } = this.state;
+
+    const response = await request
+      .get(POKEMON_API_URL)
+      .query({ pokemon: nameSearch });
+
+    const hpsOptions = [...new Set(response.body.results.map(pokemon => pokemon.hp))];
+
+    this.setState({
+      pokemon: response.body.results,
+      staticPokemon: response.body.results,
+      hp: hpsOptions
+    });
+  }
+
+  handleSearch = async ({ nameSearch, hpFilter, attackFilter, defenseFilter, sortField }) => {
+
+    const { staticPokemon } = this.state;
+    const searchedData = staticPokemon
+
+      .filter(eachPokemon => {
+        return !hpFilter || eachPokemon.hp === Number(hpFilter);
+      })
+
+      .filter(eachPokemon => {
+        return !attackFilter || eachPokemon.attack === Number(attackFilter);
+      })
+
+      .filter(eachPokemon => {
+        return !defenseFilter || eachPokemon.defense === Number(defenseFilter);
+      })
+
+      .sort((a, b) => {
+        if (a[sortField] < b[sortField]) return -1;
+        if (a[sortField] > b[sortField]) return 1;
+        return 0;
+      });
+
   }
 
   render() {
 
-    const { pokemon } = this.state;
+    const { pokemon, hp } = this.state;
+    // const { hpsOptions, attacksOptions, defensesOptions } = this.props;
 
     return (
       <div className="App">
@@ -36,7 +74,10 @@ class App extends Component {
         <section className="search-options">
           <PokemonSearch
             onSearch={this.handleSearch}
-            pokemon={pokemon} />
+            pokemon={pokemon}
+            hps={hp} />
+          {/*attacks={attacksOptions}
+    defenses={defensesOptions} />*/}
         </section>
 
         <main>
--- src/pokemon/PokemonSearch.js
+++ src/pokemon/PokemonSearch.js
@@ -4,10 +4,25 @@ import './PokemonSearch.css';
 class PokemonSearch extends Component {
   state = {
     nameSearch: '',
+    hpFilter: '',
+    attackFilter: '',
+    defenseFilter: ''
   }
 
   handleNameSearch = ({ target }) => {
     this.setState({ nameSearch: target.value });
+  }
+
+  handleHpFilter = ({ target }) => {
+    this.setState({ hpFilter: target.value });
+  }
+
+  handleAttackFilter = ({ target }) => {
+    this.setState({ attackFilter: target.value });
+  }
+
+  handleDefenseFilter = ({ target }) => {
+    this.setState({ defenseFilter: target.value });
   }
 
   handleSubmit = (e) => {
@@ -17,7 +32,8 @@ class PokemonSearch extends Component {
 
   render() {
 
-    const { nameSearch } = this.state;
+    const { nameSearch, hpFilter, attackFilter, defenseFilter, sortField } = this.state;
+    const { hps } = this.props;
 
     return (
       <form className="PokemonSearch" onSubmit={this.handleSubmit}>
@@ -27,6 +43,65 @@ class PokemonSearch extends Component {
           value={nameSearch}
           onChange={this.handleNameSearch}
         />
+
+        <p>HP:</p>
+        <select
+          name="hpFilter"
+          value={hpFilter}
+          onChange={this.handleHpFilter}
+        >
+          <option value="">Sort by...</option>
+          {hps.map(hp => (
+            <option
+              key={hp}
+              value={hp}
+            > {hp}
+            </option>
+          ))}
+        </select>
+
+        {/*<p>Attack:</p>
+        <select
+          name="attackFilter"
+          value={attackFilter}
+          onChange={this.handleAttackFilter}
+        >
+          <option value="">Sort by...</option>
+          {attacks.map(attack => (
+            <option
+              key={attack}
+              value={attack}
+            > {attack}
+            </option>
+          ))}
+        </select>
+
+        <p>Defense:</p>
+        <select
+          name="defenseFilter"
+          value={defenseFilter}
+          onChange={this.handleDefenseFilter}
+        >
+          <option value="">Sort by...</option>
+          {defenses.map(defense => (
+            <option
+              key={defense}
+              value={defense}
+            > {defense}
+            </option>
+          ))}
+        </select>
+
+        <p>All Data:</p>
+        <select
+          name="sortField"
+          value={sortField}
+          onChange={this.handleSearchChange}
+        >
+          <option value="">Sort by...</option>
+          <option value="title">title</option>
+          <option value="horns">horns</option>
+        </select> */}
 
         <button>Search</button>
 
