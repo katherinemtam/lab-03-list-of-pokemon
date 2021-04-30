554228a6da64a0c6707baa9afc48c19b6c6ad8b2
Katherine Tam <katherinemtam@gmail.com>
2021-04-29 20:38:37 -0700

    add static paging

--- src/app/Paging.js
+++ src/app/Paging.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import './Paging.css';
 
 class Paging extends Component {
-  state = {}
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.onSearch(this.state);
+  }
+
+
   render() {
+
     return (
-      <>
-      </>
+      <form className="Paging" onSubmit={this.handleSubmit}>
+
+        <button className="prev">◀</button>
+        <span>Page 1</span>
+        <button className="next">▶</button>
+
+      </form>
     );
   }
+
 }
 
 export default Paging;
