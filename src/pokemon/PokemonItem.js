6ae063da7d98ef74a46905060b120bc59032af20
Katherine Tam <katherinemtam@gmail.com>
2021-05-02 23:30:29 -0700

    change pokedex link from title to tile

--- src/pokemon/PokemonItem.js
+++ src/pokemon/PokemonItem.js
@@ -9,23 +9,25 @@ class PokemonItem extends Component {
     const className2 = pokemon.type_2;
 
     return (
-      <li className="PokemonItem">
+      <a href={pokemon.pokedex}>
+        <li className="PokemonItem">
 
-        <h2><a href={pokemon.pokedex}>{pokemon.pokemon}</a></h2>
+          <h2>{pokemon.pokemon}</h2>
 
-        <div className="types">
-          <h3 className={className1}>{pokemon.type_1}</h3>
-          <h3 className={className2}>{pokemon.type_2}</h3>
-        </div>
+          <div className="types">
+            <h3 className={className1}>{pokemon.type_1}</h3>
+            <h3 className={className2}>{pokemon.type_2}</h3>
+          </div>
 
-        <img
-          src={pokemon.url_image}
-          alt={pokemon.pokemon}
-        />
-        <p>HP: {pokemon.hp}</p>
-        <p>Attack: {pokemon.attack}</p>
-        <p>Defense: {pokemon.defense}</p>
-      </li >
+          <img
+            src={pokemon.url_image}
+            alt={pokemon.pokemon}
+          />
+          <p>HP: {pokemon.hp}</p>
+          <p>Attack: {pokemon.attack}</p>
+          <p>Defense: {pokemon.defense}</p>
+        </li >
+      </a>
     );
   }
 }
