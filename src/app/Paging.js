2705e126bb493ab548c979e2021aaa2a9a38a9b5
Katherine Tam <katherinemtam@gmail.com>
2021-05-03 00:14:19 -0700

    add perPage filter and functionality

--- src/app/Paging.js
+++ src/app/Paging.js
@@ -2,18 +2,42 @@ import React, { Component } from 'react';
 import './Paging.css';
 
 class Paging extends Component {
+  state = {
+    pokemonPerPage: 25,
+  }
+
+  handlePerPageChange = ({ target }) => {
+    this.setState({ pokemonPerPage: target.value });
+  }
 
   handleSubmit = (e) => {
     e.preventDefault();
+    this.props.onPerPageChange(this.state);
   }
 
+  componentDidUpdate(prevProp, prevState) {
+    if (prevState !== this.state) {
+      this.props.onPerPageChange(this.state);
+    }
+  }
 
   render() {
 
-    const { page, onPrev, onNext } = this.props;
+    const { pokemonPerPage, page, onPrev, onNext } = this.props;
 
     return (
       <form className="Paging" onSubmit={this.handleSubmit}>
+
+        <select
+          name="pokemonPerPage"
+          value={pokemonPerPage}
+          onChange={this.handlePerPageChange}
+        >
+          <option value="">Pokemon Per Page</option>
+          <option value="25">25</option>
+          <option value="50">50</option>
+          <option value="100">100</option>
+        </select>
 
         <button
           className="prev"
